df6df2cff1e1210819884f1bc14c94896abdfa46
HenryFBP <HenryFBP@gmail.com>
2016-03-31 20:01:43 -0500

    added...uhhhh....how...how do you code in lisp. please help.

--- connect-four.lisp
+++ connect-four.lisp
@@ -9,4 +9,12 @@
 ; .......
 ; .......
 
+
+(defconstant board-height 6)
+(defconstant board-width 7)
+
 (defconstant win-length 4)
+(setf gamerow (make-array board-width))
+
+
+(setf gameboard (make-array board-width))
